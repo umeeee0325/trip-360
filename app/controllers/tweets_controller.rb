9e71f0a0f236b64e5a74207f1a27cca87a8be6b3
umeeee0325 <nana19970204@icloud.com>
2024-02-22 13:41:47 +0900

    画像表示の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,6 +8,11 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
 
+  def create
+    Tweet.create(tweet_params)
+    redirect_to '/'
+  end
+
   private
 
   def tweet_params
