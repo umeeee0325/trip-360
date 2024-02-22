ebf6820b04bc32d6be81d6d595f30942f54082d0
umeeee0325 <nana19970204@icloud.com>
2024-02-22 12:48:15 +0900

    Active Storageの導入

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,4 +7,13 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:residence_id, :title, :text, :city, :spot, :shooting_day)
+  end
 end
+
+# .merge(user_id: current_user.id)
+
