5f4cf2c251345d4d5bc468146e79adb8fc125c17
umeeee0325 <nana19970204@icloud.com>
2024-02-27 20:38:47 +0900

    ルーティングをネストで記述

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     registrations: "users/registrations"
   }
   root to: 'tweets#index'
-  resources :tweets
+  resources :tweets do
+    resources :comments, only: :create
+  end
   resources :users, only: [:show, :edit, :update]
 end
