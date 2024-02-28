1530927f59362485e71a7b3f4efda8a8d10b0dac
umeeee0325 <nana19970204@icloud.com>
2024-02-28 18:10:07 +0900

    バリデーションの変更

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -4,11 +4,12 @@ class Tweet < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :residence
 
-  validates :residence_id, presence: true
-  validates :title, presence: true
-  validates :text, presence: true
+  validates :residence_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :text, presence: true, length: { maximum: 100 }
   validates :image, presence: true
   validates :residence_id, numericality: { other_than: 1 }
+  validates :city, :spot, length: { maximum: 30 }
 
   belongs_to :user
   has_many :comments 
