c8cfd497492b2cc3468cb8f7c6352aae7e37d67f
umeeee0325 <nana19970204@icloud.com>
2024-02-28 17:51:57 +0900

    Userモデル単体テストコード

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できるとき' do
+      it 'nicknameとemail、passwordとpassword_confirmation、profileが存在し、gender_id、residence_id、age_idが2以上だと登録できる' do
+        expect(@user).to be_valid
+      end
+      context '新規登録できないとき' do
+      it 'nicknameが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+      it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank",
+        "Password confirmation doesn't match Password",
+        "Password is too short (minimum is 6 characters)",
+        "Password は半角英数字で入力してください")
+      end
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '1234567'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      it 'nicknameが9文字以上では登録できない' do
+        @user.nickname = 'aaaaaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname is too long (maximum is 8 characters)")
+      end
+      it 'profileが16文字以上では登録できない' do
+        @user.profile = 'aaaaaaaaaaaaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Profile is too long (maximum is 15 characters)")
+      end
+      it '重複したemailが存在する場合は登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
+      it 'emailは@を含まないと登録できない' do
+        @user.email = 'testtesttest'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = 'aa11'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+      it 'passwordが129文字以上では登録できない' do
+        @user.password = Faker::Internet.password(min_length: 129, max_length: 150)
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too long (maximum is 128 characters)")
+      end
+    end
+  end
+end
